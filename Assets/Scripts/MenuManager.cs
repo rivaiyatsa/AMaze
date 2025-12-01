a728f0f63c6fb0b9d4df7141c1cabde622621d15
rivaiyatsa <rivaaiyatsaa@gmail.com>
2025-12-02 00:15:18 +0700

    map hijau dan ilangin fonts

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,13 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    // Memuat scene Settings
+    // memuat scene mapping saat tombol play ditekan
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Mapping");
+    }
+
+    // memuat scene settings
     public void LoadSettingsScene()
     {
         SceneManager.LoadScene("Settings");
     }
 
-    // Keluar dari aplikasi
+    // keluar dari aplikasi
     public void ExitGame()
     {
         #if UNITY_EDITOR
